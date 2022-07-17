899b744a27c509bf80eccd239cca97b3e9d1c7b9
Shubhankar Das <76389456+SmartTech-SD@users.noreply.github.com>
2022-07-17 10:35:18 +0530

    Update first_streamlit_app.py
    added line to import streamlit 
    added a title

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title("This Is A Beautiful Morning")
+
+streamlit.header("Sights")
+
+streamlit.text("Calm Nature")
+streamlit.text("Sweet Smell")
+streamlit.text("Good Vibe")
+
