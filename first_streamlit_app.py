96d90565297b8f04a6fd02e842d7b28b5931cabf
Shubhankar Das <76389456+SmartTech-SD@users.noreply.github.com>
2022-07-17 10:47:31 +0530

    Update first_streamlit_app.py
    changes

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -7,3 +7,13 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.title("My Mom's new healthy Diner")
+
+streamlit.header(' Breakfast favorite')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Tost')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
